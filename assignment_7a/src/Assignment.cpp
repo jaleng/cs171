e55201e94c9bb91e97847c0af6919ebf3e740915
Jalen Green <jpgreen@caltech.edu>
2015-12-11 06:12:03 -0800

    Impl inside-out test, buildPATs

--- assignment_7a/src/Assignment.cpp
+++ assignment_7a/src/Assignment.cpp
@@ -69,7 +69,48 @@ Matrix<double, 4, 4> tfmvec2mat(const vector<Transformation>& tfmvec) {
   return mat;
 }
 
-// TODO: inside-outside test
+/** Superquadric inside-outside test **/
+double sq_io(double x, double y, double z, double e, double n) {
+  return   pow(pow(x*x, 1.0/e) + pow(y*y, 1.0/e), e/n)
+         + pow(z*z, 1/n)
+         - 1;
+}
+
+struct PAT{
+  Primitive prm
+  Matrix<double, 4, 4> tfm;
+};
+
+// TODO: build vector of primitives traversing tree
+std::unique_ptr<vector<PrimAndTfm>> buildPATs (const Renderable& root, int level=0) {
+  vector<PAT> v;
+  if (level > 20) {
+    return std::move(v)
+  }
+  switch(root.getType()) {
+  case PRM:
+    //// TODO: case when renderable is primitive
+    Matrix<double, 4, 4> m;
+    m.setIdentity();
+    v.emplace_back(dynamic_cast<Primitive>(root), m);
+    break;
+  case OBJ:
+    //// TODO: case when renderable is Object
+    auto overall_tfm = tfmvec2mat(root.getOverallTransformation());
+    for (const auto& item : root.getChildren()) {
+      auto child = item.second();
+      auto child_tfm = tfmvec2mat(child.transformations);
+      auto child_made_pat_vec = buildPATs(child, level + 1);
+      for (auto& child_made_pat : child_made_pat_vec) {
+        v.push_back(overall_tfm * child_tfm * child_made_pat.tfm);
+      }
+    }
+    break;
+  case MSH:
+    assert(false);
+    break;
+  }
+}
 
 void Assignment::drawIOTest() {
   // TODO: Build vector of primitives by traversing tree
