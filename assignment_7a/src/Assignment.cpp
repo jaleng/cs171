4cc90af757fec967e00e7abcec8c368d0836e8af
Jalen Green <jpgreen@caltech.edu>
2015-12-14 00:28:56 -0800

    Start writing drawIntersectTest, fix compile errors; NOT WORKING

--- assignment_7a/src/Assignment.cpp
+++ assignment_7a/src/Assignment.cpp
@@ -8,6 +8,8 @@
 
 #include <vector>
 #include <memory>
+#include <algorithm>
+#include <limits>
 
 /** Take a Transform and create a transformation matrix **/
 Matrix<double, 4, 4> tfm2mat(const Transformation& tfm) {
@@ -71,6 +73,19 @@ Matrix<double, 4, 4> tfmvec2mat(const vector<Transformation>& tfmvec) {
   return mat;
 }
 
+/** Take a vector of transforms and create a transformation matrix **/
+Matrix<double, 4, 4> tfmvec2mat_wo_tl(const vector<Transformation>& tfmvec) {
+  Matrix<double, 4, 4> mat;
+  mat.setIdentity();
+  for (auto it = tfmvec.cbegin(); it != tfmvec.cend(); ++it) {
+    auto tmp = mat;
+    if (it->type != TRANS) {
+      mat = tfm2mat(*it) * tmp;
+    }
+  }
+  return mat;
+}
+
 /** Superquadric inside-outside test
  *  < 0 -> inside object
  *  = 0 -> on object's surface
@@ -85,8 +100,10 @@ double sq_io(double x, double y, double z, double e, double n) {
 struct PAT {
   Primitive prm;
   Matrix<double, 4, 4> tfm;
-  PAT(const Primitive& _prm, const Matrix<double, 4, 4> _tfm)
-    : prm{_prm}, tfm{_tfm}{}
+  Matrix<double, 4, 4> twot;
+  PAT(const Primitive& _prm, const Matrix<double, 4, 4> _tfm,
+      const Matrix<double, 4, 4> _twot)
+    : prm{_prm}, tfm{_tfm}, twot{_twot}{}
 };
 
 // draw blue dot at (x,y,z)
@@ -129,20 +146,23 @@ std::unique_ptr<vector<PAT>> buildPATs(const Renderable& root, int level = 0) {
     {
     Matrix<double, 4, 4> m;
     m.setIdentity();
-    v->emplace_back(dynamic_cast<const Primitive&>(root), m);
+    v->emplace_back(dynamic_cast<const Primitive&>(root), m, m);
     break;
     }
   case OBJ:
     {
     auto obj = dynamic_cast<const Object&>(root);
     auto overall_tfm = tfmvec2mat(obj.getOverallTransformation());
+    auto overall_twot = tfmvec2mat_wo_tl(obj.getOverallTransformation());
     for (const auto& item : obj.getChildren()) {
       auto child = item.second;
       auto child_tfm = tfmvec2mat(child.transformations);
+      auto child_twot = tfmvec2mat_wo_tl(child.transformations);
       auto child_made_pat_vec = buildPATs(*Renderable::get(child.name), level + 1);
       for (auto& child_made_pat : *child_made_pat_vec) {
         v->emplace_back(child_made_pat.prm,
-                        overall_tfm * child_tfm * child_made_pat.tfm);
+                        overall_tfm * child_tfm * child_made_pat.tfm,
+                        overall_twot * child_twot * child_made_pat.twot);
       }
     }
     }
@@ -208,6 +228,77 @@ void Assignment::drawIOTest() {
   }
 }
 
+// Assumes discriminant > 0
+double tminus(double a, double b, double c) {
+  auto disc = b*b - 4*a*c;
+  return (-b - sqrt(disc)) / (2*a);
+}
+
+
+// Assumes discriminant > 0
+double tplus(double a, double b, double c) {
+  auto disc = b*b-4*a*c;
+  return (2*c) / (-b - sqrt(disc));
+}
+
+// may not be normalized
+Vector3d grad_sq_io(double x, double y, double z, double e, double n) {
+  Vector3d res;
+  auto blah = pow(pow(x*x, 1/e) + pow(y*y, 1/e),
+                  e/n - 1);
+  res(0) = (2*x*pow(x*x, 1/e-1)*blah) / n;
+  res(1) = (2*y*pow(y*y, 1/e-1)*blah) / n;
+  res(2) = 2*z*pow(z*z, 1/n-1) / n;
+  return res;
+}
+
+struct MissOrHit {
+  bool hit;
+  double t;
+  MissOrHit(bool _hit, double _t) : hit{_hit}, t{_t}{}
+};
+
+MissOrHit findIntersection(double e, double n,
+                           Vector3d a, Vector3d b, double t_old) {
+  auto ray = [a, b](double t) {
+    return a*t + b;
+  };
+
+  auto sq_io_r = [a, b, e, n](Vector3d r) {
+    return sq_io(r(0), r(1), r(2), e, n);
+  };
+
+  auto grad_sq_io_r = [e, n](Vector3d r) {
+    return grad_sq_io(r(0), r(1), r(2), e, n);
+  };
+
+  auto g = [a, b, e, n, sq_io_r, ray](double t) {
+    return sq_io_r(ray(t));
+  };
+
+  auto gp = [a, b, e, n, grad_sq_io_r, ray](double t) {
+    return a.dot(grad_sq_io_r(ray(t)));
+  };
+
+  auto gp_was_negative = gp(t_old) < 0;
+  for (int iteration = 0; iteration < 10000; ++iteration) {
+    auto gpt = gp(t_old);
+    auto gt = g(t_old);
+
+    if (abs(gt) < 0.00001) {
+      return MissOrHit(true, t_old);
+    } else if (abs(gpt) < 0.00001){
+      return MissOrHit(false, 0);
+    } else if (gp_was_negative && gpt > 0) {
+      // gp sign changed from negative to positive
+      return MissOrHit(false, 0);
+    }
+    gp_was_negative = gpt < 0;
+    t_old = t_old - (gt / gpt);
+  }
+  assert(false);  // Did not converge or return miss
+}
+
 void Assignment::drawIntersectTest(Camera *camera) {
   /*
     for each sq in the scene:
@@ -216,14 +307,111 @@ void Assignment::drawIntersectTest(Camera *camera) {
       
     draw line from closest intersection in direction of normal
   */
-  const Line* cur_state = CommandLine::getState();
-  Renderable* ren = nullptr;
-  if (cur_state) {
-    ren = Renderable::get(cur_state->tokens[1]);
-  } else {
-    return;
-  }
+  // Get b (camera position)
+  // Vector3f
+  auto b_v = camera->getPosition();
+  auto a_v = camera->getAxis();
+  /*
+  Matrix<double, 4, 1> a0_mat;
+  a0_mat << 0, 0, -1, 1;
+
+  // Apply camera rotation to a0 to get a
+  auto rot_mat = tfm2mat(Transformation(
+                           ROTATE, axis[0], axis[1], axis[2], angle));
+  Matrix<double, 4, 1> a_mat;
+  a_mat = rot_mat * a0_mat;
+  auto a = Vector3d(a_mat(0), a_mat(1), a_mat(2));
+  */
+
+  double lowest_t = std::numeric_limits<double>::infinity();
+  PAT* closest_pat = nullptr;
 
   // Build vector of PATs by traversing tree
-  auto pats = buildPATs(*ren);
+  auto pats = getpats();
+  for (auto& pat : *pats) {
+    //// TODO: test if primitive is hit by camera look ray
+
+    // Transform a_v and b_v using the primitive's transform
+    Matrix<double, 4, 1> am;
+    am << a_v(0), a_v(1), a_v(2), 1;
+    Matrix<double, 4, 1> bm;
+    bm << b_v(0), b_v(1), b_v(2), 1;
+
+    auto atm = pat.tfm * am;
+    Vector3d at(atm(0), atm(1), atm(2));
+    auto btm = pat.tfm * bm;
+    Vector3d bt(btm(0), btm(1), btm(2));
+
+    auto a = a_v.dot(a_v);
+    auto b = 2*(a_v.dot(b_v));
+    auto c = b_v.dot(b_v) - 3.0;
+
+    auto discriminant = b*b - 4*a*c;
+    if (discriminant < 0) {
+      continue;
+    }
+
+    auto tp = tplus(a, b, c);
+    auto tm = tminus(a, b, c);
+
+    if (tp < 0 && tm < 0) {
+      continue;
+    } else if (tp > 0 && tm > 0) {
+      auto tmc = findIntersection(pat.prm.getExp0(),
+                                  pat.prm.getExp1(),
+                                  at,
+                                  bt,
+                                  tm);
+      assert(tmc.hit == true);
+      lowest_t = min(lowest_t, tmc.t);
+    } else {
+      // Find intersection using tp
+      auto tpc = findIntersection(pat.prm.getExp0(),
+                                  pat.prm.getExp1(),
+                                  at,
+                                  bt,
+                                  tp);
+
+      // Find intersection using tm
+      auto tmc = findIntersection(pat.prm.getExp0(),
+                                  pat.prm.getExp1(),
+                                  at,
+                                  bt,
+                                  tm);
+
+      if (tpc.hit && tmc.hit && tpc.t > 0 && tmc.t > 0) {
+        // use lowest tc
+        auto tf = min(tpc.t, tmc.t);
+        if (tf < lowest_t) {
+          lowest_t = tf;
+          closest_pat = &pat;
+        }
+      } else if (tpc.hit && tpc.t > 0) {
+        if (tpc.t < lowest_t) {
+          lowest_t = tpc.t;
+          closest_pat = &pat;
+        }
+        continue;
+      } else if (tmc.hit && tmc.t > 0) {
+        if (tmc.t < lowest_t) {
+          lowest_t = tmc.t;
+          closest_pat = &pat;
+        }
+      }
+    }
+  }
+  if (closest_pat != nullptr) {
+    // Get the normal, apply inverse transform (normal form), then draw line
+    auto v = a_v * lowest_t + b_v;
+    Matrix<double, 4, 1> v_m;
+    v_m << v(0), v(1), v(2), 1;
+    Matrix<double, 4, 1> vt_m = closest_pat->tfm.inverse() * v_m;
+    double w = vt_m(3);
+    Vector3d vt(vt_m(0)/w, vt_m(1)/w, vt_m(2)/w);
+    auto nt = closest_pat->prm.getNormal(v);
+    //auto n = ?normalinverse(nt);
+
+    // TODO: Draw line from intersection point in direction of normal
+    
+  }
 }
